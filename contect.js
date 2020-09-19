5ce4c0efc280abd8d55db0b368881777a1c93e08
Romeo Namunde <romeomureithi@gmail.com>
2020-09-19 12:08:32 +0300

    Added a bit of backend sites

--- contect.js
+++ contect.js
@@ -1,3 +1,27 @@
 // coded by @mkyd-kill
 
+const { url } = require("inspector");
+
 const projectName = 'kalax-training-center';
+
+$('#contact-form').submit(function(e) {
+    var name = document.getElementById('name'),
+        email = document.getElementById('email'),
+        subject = document.getElementById('messageSubject'),
+        message = document.getElementById('comment');
+
+    if (!name.value || !email.value || !subject.value || !message.value) {
+        alertify.error('Please check your entries')
+    }
+    else {
+        $.ajax({
+            url: "https://formspree.io/romeomureithi@gmail.com",
+            method: "POST",
+            data: $('#contact-form').serialize(),
+            datatype: "json"
+        });
+        e.preventDefault()
+        $(this).get(0).reset()
+        alertify.success('Message Sent')
+    }
+})

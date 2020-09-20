6e034d62e3b0a7c00f2e51cb681e72789e5aebf0
Romeo Namunde <romeomureithi@gmail.com>
2020-09-20 16:19:44 +0300

    Start of an advertisment navbar

--- contect.js
+++ contect.js
@@ -13,26 +13,26 @@ $('#contact-form').submit(function(e) {
     }
     else {
         // email validation
-    function validate(){
-        var email = document.getElementById('email').value;
+        function validate(){
+            var email = document.getElementById('email').value;
 
-        var regx = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9\.-]+).([a-z]{2, 8})(.[a-z]{2, 8})$/;
-            if (regx.test(email)){
-                $.ajax({
+            var regx = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9\.-]+).([a-z]{2, 8})(.[a-z]{2, 8})$/;
+                if (regx.test(email)){
+                    $.ajax({
                     url: "https://formspree.io/romeomureithi@gmail.com",
                     method: "POST",
                     data: $('#contact-form').serialize(),
                     datatype: "json"
-                });
-                e.preventDefault()
-                $(this).get(0).reset()
-                alertify.success('Message Sent')
-            }
-            else {
-                document.getElementById('emailtext').innerHTML="Invalid Email";
-                document.getElementById('emailtext').style.visibility="visible";
-                document.getElementById('emailtext').style.color="red";
-            }
-    }
+                    });
+                    e.preventDefault()
+                    $(this).get(0).reset()
+                    alertify.success('Message Sent')
+                }
+                else {
+                    document.getElementById('emailtext').innerHTML="Invalid Email";
+                    document.getElementById('emailtext').style.visibility="visible";
+                    document.getElementById('emailtext').style.color="red";
+                }
+        }
     }
 })
